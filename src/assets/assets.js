ff398c89ee769e6ee3e7f2b1418dffee3367fe95
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-12-09 05:05:07 +0300

    Updates

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,1 +1,169 @@
-import eminem from './69824 2.png';
+import img1 from './69824 2.png';
+import img2 from './69824 3.png';
+import img3 from './69824 4.png';
+import img4 from './69824 5.png';
+import img5 from './69824 6.png';
+import img6 from './69824 7.png';
+import img7 from './Frame 63.png';
+import img8 from './Frame 64.png';
+import img9 from './Frame 65.png';
+import img10 from './Frame 66.png';
+import img11 from './69824 6.png';
+import img12 from './Frame 185 (1).png';
+import img13 from './Frame 185 (2).png';
+import img14 from './Frame 185 (3).png';
+import img15 from './Frame 185 (4).png';
+import img16 from './Frame 185 (5).png';
+import img17 from './Frame 185 (6).png';
+import img18 from './Frame 185.png';
+import img19 from './Rectangle 17 (1).png';
+import img20 from './Rectangle 17 (2).png';
+import img21 from './Rectangle 17.png';
+
+import song1 from './song1.m4a';
+import song2 from './song2.mp3';
+import song3 from './song3.m4a';
+
+import hero from './Hero Section.png';
+
+
+export const assets = {
+   img1, 
+   img2,
+   img3,
+   img4,
+   img5,
+   img6, 
+   img7,
+   img8,
+   img9,
+   img10,
+   img11, 
+   img12,
+   img13,
+   img14,
+   img15,
+   img16, 
+   img17,
+   img18,
+   img19,
+   img20,
+img21,
+song1,
+song2,
+song3,
+hero
+}
+
+export const albumsData = [
+    {
+        id: 0,
+        name: "Top 50 Global",
+        image: img1,
+        desc: "Your weeky update of the most played tracks",
+        bgColor: "#2a4365"
+    },
+    {
+        id:1,
+        name: "Top 50 Ethiopia",
+        image: img2,
+        desc: "Your weeky update of the most played tracks",
+        bgColor: "#22543d"
+
+    },
+    {
+        id:2,
+        name: "Trending Ethiopia",
+        image: img3,
+        desc:"Your weeky update of the most played tracks",
+        bgColor: "#742a2a"
+    },
+    {
+        id:3,
+        name: "Trending Global",
+        image: img4,
+        desc: "Your weeky update of the most played tracks",
+        bgColor: "#44337a"
+    },
+    {
+        id:4,
+        name:"Mega Hits",
+        image: img5,
+        desc: "Your weeky update of the most played tracks",
+        bgColor:"#234e52"
+    },
+    {
+        id:5,
+        name: "Happy Favorites",
+        image: img6,
+        desc:"Your weeky update of the most played tracks",
+        bgColor:"#744210"
+    }
+]
+
+export const songsData = [
+    {
+        id:0,
+        name:"Song One",
+        image: img1,
+        file: song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "3:00"
+    },
+       {
+        id:1,
+        name:"Song Two",
+        image: img2,
+        file: song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "4:00"
+    },
+       {
+        id:2,
+        name:"Song Three",
+        image: img3,
+        file: song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "3:00"
+    },
+       {
+        id:3,
+        name:"Song Four",
+        image: img4,
+        file: song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "4:00"
+    },
+       {
+        id:4,
+        name:"Song Five",
+        image: img5,
+        file: song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "2:00"
+    },
+       {
+        id:5,
+        name:"Song Six",
+        image: img6,
+        file: song3,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "3:00"
+    },
+       {
+        id:6,
+        name:"Song Seven",
+        image: img7,
+        file: song1,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "4:00"
+    },
+       {
+        id:7,
+        name:"Song Eight",
+        image: img8,
+        file: song2,
+        desc:"Put a smile on your face with these happy tunes",
+        duration: "5:00"
+    },
+]
