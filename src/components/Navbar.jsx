35c5dac3e108869e64b1daf6005eef5ce2759644
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-12-13 05:22:08 +0300

    updates

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'
+import { MdOutlineKeyboardArrowLeft, MdOutlineKeyboardArrowRight } from 'react-icons/md'
+
+const Navbar = () => {
+  return (
+    <>  
+    <div className='w-full flex justify-between text-center font-semibold'>
+      <div className='flex items-center gap-2'>
+        <MdOutlineKeyboardArrowLeft className='bg-black p-2 rounded-2xl cursor-pointer' size={35}/>
+        <MdOutlineKeyboardArrowRight className='bg-black p-2 rounded-2xl cursor-pointer' size={35}/>
+
+      </div>
+
+<div className='flex items-center gap-4'>
+    <p className='bg-white text-black text-[15px] px-4 
+    py-1 rounded-2xl hidden md:block cursor-pointer'>Explore Premium</p>
+    <p className='bg-black py-1 px-4 rounded-2xl text-[15px] cursor-pointer'>Install App</p>
+    <p className='bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center '>T</p>
+</div>
+</div>
+
+<div className='flex items-center gap-2 mt-4'>
+    <p className='bg-white text-black px-4 py-1 rounded-2xl cursor-pointer'>All</p>
+     <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Music</p>
+     <p className='bg-black px-4 py-1 rounded-2xl cursor-pointer'>Podcasts</p>
+</div>
+
+
+    
+    </>
+  
+  )
+}
+
+export default Navbar
