720e70ffdc6970d26457f688edec02f46784e240
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-12-14 05:30:03 +0300

    Add Album Item

---
+++ src/components/DisplayAlbum.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from './Navbar'
+import { useParams } from 'react-router-dom'
+import { albumsData } from '../assets/assets';
+
+const DisplayAlbum = () => {
+
+    const {id} = useParams();
+    const albumData = albumsData[id];
+  return (
+    <>
+      <Navbar />
+      <div className='mt-10 flex gap-8 flex-col md:flex-row md:items-end'>
+           <img src={albumData.img} alt="" />
+      </div>
+    </>
+  )
+}
+
+export default DisplayAlbum
